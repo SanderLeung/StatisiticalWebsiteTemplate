5f27d8967415efb0815b6aae13739576b42eb6e5
Sander Leung <sander.leung@gmail.com>
2023-06-02 19:36:45 -0700

    pull data from API

--- GetFixtures.py
+++ GetFixtures.py
@@ -21,25 +21,40 @@ def get_fixtures(dataset):
     
     return(df)
 
-#SF_data = get_fixtures("88g8-5mnd")
-SF_data = pd.read_csv('.\Employee_Compensation.csv')
-SF_data.name = 'Compensation data'
+#SF_data = pd.read_csv('.\Employee_Compensation.csv')
+#SF_data.name = 'Compensation data'
 
-def plot_fixture_data(df, code):
-    jf_group = df.query(f"job_family_code == '{code}'")
-    jf_group.boxplot(column='total_salary', by='job_code', figsize=(50,20))
-    fig, ax = plt.subplots(figsize = (25,8))
-    fig.tight_layout()
+def get_sf_data():
+    df = get_fixtures("88g8-5mnd")
+    df["total_salary"] = df["total_salary"].astype(float)
+    df["overtime"] = df["overtime"].astype(float)
+    df["salaries"] = df["salaries"].astype(float)
+    return df
+
+def create_figure(df, code):
+    fig, ax = plt.subplots(figsize = (20,10))
     fig.patch.set_facecolor('#E8E5DA')
-    plt.savefig('static/images/plot_for_{}.png'.format(df.name),bbox_inches = 'tight', facecolor=fig.get_facecolor())
-    return 
+    jf_group = df.query(f"job_family_code == '{code}'")
+    
+    x = sorted(jf_group['job_code'].unique())
+    data = [jf_group.query("job_code == @job_code")['total_salary'] for job_code in x]
+
+    if x:
+        ax.boxplot(data, labels = x)
+
+    plt.xticks(rotation = 30, size = 10)
+    plt.xlabel("Job Code", size = 15)
+    plt.ylabel("Salary", size = 10)
+    plt.title("Salary Dist by Job", size = 20)
+
+    return fig
 
 #fig size for insta = (25,8) and for website = (15,8)
 
 def overtime_data(df):
-    overtime = df.query('Overtime > Salaries and Salaries > 0')
-    overtime['overtime_diff'] = overtime['Overtime'] / overtime['Salaries']
-    res = overtime.groupby('Job')['overtime_diff'].mean().sort_values(ascending=False)
+    overtime = df.query('overtime > salaries and salaries > 0')
+    overtime['overtime_diff'] = overtime['overtime'] / overtime['salaries']
+    res = overtime.groupby('job')['overtime_diff'].mean().sort_values(ascending=False)
     return res.to_frame()
 
     
--- compensation.py
+++ compensation.py
@@ -13,13 +13,12 @@ import matplotlib.pyplot as plt
 
 #other requirements
 import io
-import random
 
 #Data imports
-#from GetFixtures import SF_data, SF_Overtime
-from GetFixtures import overtime_data
-SF_data = pd.read_csv('.\Employee_Compensation.csv')
-SF_Overtime = overtime_data(SF_data)
+from GetFixtures import *
+#SF_data = pd.read_csv('.\Employee_Compensation.csv')
+SF_Data = get_sf_data()
+SF_Overtime = overtime_data(SF_Data)
 
 app = Flask(__name__)
 
@@ -42,28 +41,10 @@ def legend():
 
 @app.route('/plot/<code>')
 def plot_png(code=1000):
-    fig = create_figure(code)
+    fig = create_figure(SF_Data, code)
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
-
-def create_figure(code):
-    fig, ax = plt.subplots(figsize = (20,10))
-    fig.patch.set_facecolor('#E8E5DA')
-    jf_group = SF_data.query(f"`Job Family Code` == '{code}'")
-    
-    x = sorted(jf_group['Job Code'].unique())
-    data = [jf_group.query("`Job Code` == @job_code")['Total Salary'] for job_code in x]
-
-    if x:
-        ax.boxplot(data, labels = x)
-
-    plt.xticks(rotation = 30, size = 10)
-    plt.xlabel("Job Code", size = 15)
-    plt.ylabel("Salary", size = 10)
-    plt.title("Salary Dist by Job", size = 20)
-
-    return fig
 
 if __name__ == '__main__':
     app.run(debug = True)
